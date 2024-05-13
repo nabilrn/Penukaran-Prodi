53d883f78230893440462230239222773de2d1b6
trianazahara <trianazahara03@gmail.com>
2024-05-13 11:47:46 +0700

    view profile

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,7 +33,6 @@ const checklogin = async (req, res) => {
     // Set cookie dengan token
     res.cookie("token", token, { httpOnly: true });
 
-
     // Redirect ke halaman sesuai dengan peran pengguna
     if (user.role == "mahasiswa") {
       return res.redirect("/mahasiswa/home");
@@ -50,6 +49,41 @@ const checklogin = async (req, res) => {
   }
 };
 
+const changeProfile = async (req, res) => {
+  const user = await User.findByPk(req.userId);
+  res.render("edit_profile", { user });
+  // return user;
+  // console.log(req.cookies.token)
+};
+
+const view_profile = async (req, res) => {
+  const profile = await User.findByPk(req.userId);
+  res.render("account", { profile });
+
+};
+
+
+const editProfile = async (req, res) => {
+  try {
+    const { newUsername } = req.body;
+    if (!newUsername) {
+      return res.status(400).json({ message: "Username baru tidak valid" });
+    }
+
+    const user = await User.findByPk(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: "Pengguna tidak ditemukan" });
+    }
+
+    await user.update({ username: newUsername });
+
+    return res.status(200).json({ message: "Profil berhasil diperbarui" });
+  } catch (error) {
+    console.error("Error updating profile: ", error);
+    return res.status(500).json({ message: "Terjadi kesalahan server" });
+  }
+};
+
 function logout(req, res) {
   res.clearCookie("token");
   res.redirect("/login");
@@ -58,5 +92,8 @@ function logout(req, res) {
 module.exports = {
   checklogin,
   logout,
-  form
+  form,
+  view_profile,
+  editProfile,
+  changeProfile,
 };
