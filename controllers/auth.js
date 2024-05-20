33d727821068bda3a076c8f2c64ca5e00927fb5a
nabil260904 <116415990+nabil260904@users.noreply.github.com>
2024-05-20 21:03:18 +0700

    udpate function checklogin and login.ejs

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,40 +9,35 @@ const form = (req, res) => {
 const checklogin = async (req, res) => {
   const { username, password } = req.body;
   try {
-    // Menggunakan nama variabel lain untuk menyimpan hasil pencarian user
     const user = await User.findOne({ where: { username } });
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Verifikasi password
     const isValidPassword = await bcrypt.compare(password, user.password);
 
     if (!isValidPassword) {
       return res.status(401).json({ message: "Invalid password" });
     }
 
-    // Buat token JWT
     const token = jwt.sign(
       { id: user.id, username: user.username, role: user.role },
       process.env.JWT_SECRET_TOKEN,
       { expiresIn: 86400 }
     );
 
-    // Set cookie dengan token
     res.cookie("token", token, { httpOnly: true });
 
-    // Redirect ke halaman sesuai dengan peran pengguna
-    if (user.role == "mahasiswa") {
-      return res.redirect("/mahasiswa/home");
-    } else if (user.role == "admin") {
-      return res.redirect("/admin/dashboard");
-    } else {
-      res.status(200).send({ auth: true, token: token });
+    let redirectUrl = '/';
+    if (user.role === 'mahasiswa') {
+      redirectUrl = '/mahasiswa/home';
+    } else if (user.role === 'admin') {
+      redirectUrl = '/admin/dashboard';
     }
 
-    console.log(user.role);
+    return res.status(200).json({ auth: true, token: token, redirectUrl: redirectUrl });
+
   } catch (err) {
     console.error("Error during login: ", err);
     res.status(500).json({ message: "Internal server error" });
