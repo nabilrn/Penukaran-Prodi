e5889a0fea695a24976bc96c0f9ad895d42dec2c
natiarap <144250075+natiarap@users.noreply.github.com>
2024-06-21 19:52:42 +0700

    fungsional

--- models/feedback.js
+++ models/feedback.js
@@ -13,7 +13,8 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Feedback.init({
+  Feedback.init(
+    {
     pesan: DataTypes.STRING,
   }, {
     sequelize,
